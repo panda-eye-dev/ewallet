f437ebf3d591a97b041776141988dabaf62793c3
Payal <payalkumavat02@gmail.com>
2026-02-10 17:52:30 +0530

    update api url to cloudflare tunnel

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000",
+  baseURL: "https://promises-tires-periodically-wisdom.trycloudflare.com",
+
 });
 
 client.interceptors.request.use((config) => {
