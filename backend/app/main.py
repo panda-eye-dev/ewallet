b103864922305db001ba586ebe5d659de13c4cc0
Payal <payalkumavat02@gmail.com>
2026-02-10 17:09:31 +0530

    fix vercel routing

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,7 +5,6 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173","http://localhost:8000" ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
